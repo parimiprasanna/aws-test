94c89e8b0463a7b259aebb0ec6385188c5278c1b
Ubuntu <ubuntu@ip-172-31-11-111.ap-southeast-2.compute.internal>
2023-12-30 10:46:12 +0000

    deploy-update

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,58 @@
-echo "from aws environment"
-echo "test from aws"
+pipeline {
+    agent any
+
+    stages {
+        stage('git-checkout-files') {
+            steps {
+            echo "checkout all the files";
+		    git url: 'https://github.com/parimiprasanna/aws-test.git', branch: 'main'
+            }
+        }
+		
+stage('Development') {
+        steps {
+            sh 'bash deploy.sh'
+		    echo "Deployed successfully";
+		    
+		}
+	  }
+		stage('Test') {
+	    steps {
+	        sh 'bash deploy.sh'
+		    echo " Deploy successful on Test";
+		}
+	  }
+	
+	stage('Preprod') {
+	    steps {
+	        sh  'bash deploy.sh'
+		    echo " Deploy successful on Test";
+		}
+	  }
+	stage('Production') {
+	    steps {
+	        sh 'bash deploy.sh'
+		    echo " Deploy successful on Test";
+		}
+	  }
+	 }
+post {
+	      always {
+		      echo "This will always run"
+		  }
+     success {
+	         echo "This will run on if successfull"
+		  }
+		  
+	 failure {
+	         echo "This will run on if failed"
+		  }	  
+		  
+	 unstable {
+	         echo "This will run on if the run was marked as unstable"
+		  }	  
+		  	  
+	 changed {
+	         echo "This will run only if the state of the pipeline changed"
+			 echo "For example, if the Pipline was previously failing but is now successful"
+		  }
